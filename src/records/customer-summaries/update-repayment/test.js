51eba0ce482df6b8208fbaf0a577bd1a0c5e1baa
Angello Obel <angello@obel.co>
2020-01-28 17:52:49 +0300

    can update repayment summaries

--- src/records/customer-summaries/update-repayment/test.js
+++ src/records/customer-summaries/update-repayment/test.js
@@ -1,7 +1,55 @@
-import updateRepayment from ".";
+import updateRepaymentSummary from ".";
+import { fetchAll } from "../fetch-summaries";
 
-describe('updateRepayment', () => {
+const customer = 2;
+const season = 110;
+const amount = 200
+const mockCustomerSummaries = [
+  {
+    "CustomerID": 1,
+    "SeasonID": 110,
+    "Credit": 7900,
+    "TotalRepaid": 7900
+  },
+  {
+    "CustomerID": customer,
+    "SeasonID": season,
+    "Credit": 7900,
+    "TotalRepaid": 6700
+  },
+  {
+    "CustomerID": 1,
+    "SeasonID": 130,
+    "Credit": 8000,
+    "TotalRepaid": 7400
+  },
+  {
+    "CustomerID": customer,
+    "SeasonID": 130,
+    "Credit": 7900,
+    "TotalRepaid": 7900
+  },
+]
+describe('updateRepaymentSummary', () => {
+  beforeAll(() => {
+    localStorage.setItem('CustomerSummaries', JSON.stringify(mockCustomerSummaries))
+  });
   it('should be a function', () => {
-    expect(updateRepayment).toBeInstanceOf(Function);
+    expect(updateRepaymentSummary).toBeInstanceOf(Function);
+  });
+  it('should increment the TotalRepaid amount in the entry matching the customerID and seasonID', () => {
+    updateRepaymentSummary(customer, season, amount);
+    const summaries = fetchAll();
+    expect(summaries[1]).toEqual({
+      CustomerID: customer,
+      SeasonID: season,
+      Credit: mockCustomerSummaries[1].Credit,
+      TotalRepaid: mockCustomerSummaries[1].TotalRepaid + amount,
+    });
+  });
+  describe.skip('Edge cases', () => {
+    it('should do something when no entry is found to update', () => {
+      jest.fail('Not implemented yet')
+    });
   });
 });
