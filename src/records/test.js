ef6810c70c5129f70777fde013eab2ff0ef5909d
Angello Obel <angello@pop-os.localdomain>
2020-01-28 14:50:41 +0300

    support for cascading payments

--- src/records/test.js
+++ src/records/test.js
@@ -14,7 +14,7 @@ const mockAmount = '990';
 
 describe('records', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
+    jest.resetAllMocks();
   });
   it('should have an add payment function', () => {
     expect(addPayment).toBeInstanceOf(Function);
@@ -34,7 +34,7 @@ describe('records', () => {
       });
     });
     describe('when overpaid for all seasons', () => {
-      const outStandingSeason = { seasonID: 200, outStandingAmount: 0 };
+      const outStandingSeason = { seasonID: 200, outstandingAmount: 0 };
       beforeEach(() => {
         getOutstandingSeason.mockReturnValue(outStandingSeason)
       });
@@ -54,5 +54,54 @@ describe('records', () => {
           mockAmount);
       });
     });
+  });
+  describe('When more than enough for a single season', () => {
+    beforeEach(() => {
+      getOutstandingSeason.mockReturnValueOnce({ seasonID: 2011, outstandingAmount: 20 })
+      getOutstandingSeason.mockReturnValueOnce({ seasonID: 2012, outstandingAmount: 90 })
+      getOutstandingSeason.mockReturnValueOnce({ seasonID: 2013, outstandingAmount: 0 })
+    });
+    it('should record the full payment to the earliest outstanding season', () => {
+      addPayment(mockCustomerID, mockDate, 60);
+      expect(saveRepayment).toHaveBeenCalledWith(60, 2011, mockCustomerID, mockDate);
+    });
+    it('should record an adjustment payment to the earliest outstanding season so that only the owed amount is paid', () => {
+      addPayment(mockCustomerID, mockDate, 60);
+      expect(saveRepayment).toHaveBeenCalledWith(20 - 60, 2011, mockCustomerID, mockDate);
+    });
+    it('should record a payment with the balance to the next outstanding season', () => {
+      addPayment(mockCustomerID, mockDate, 60);
+      expect(saveRepayment).toHaveBeenCalledWith(60 - 20, 2012, mockCustomerID, mockDate);
+    });
+    it('should save only three payment record', () => {
+      addPayment(mockCustomerID, mockDate, 60);
+      expect(saveRepayment).toHaveBeenCalledTimes(3);
+    });
+  });
+  describe('For Exact payment', () => {
+    beforeEach(() => {
+      getOutstandingSeason.mockReturnValueOnce({ seasonID: 2011, outstandingAmount: 20 })
+    });
+    it('should record the full payment to the earliest outstanding season', () => {
+      addPayment(mockCustomerID, mockDate, 20);
+      expect(saveRepayment).toHaveBeenCalledWith(20, 2011, mockCustomerID, mockDate);
+    });
+    it('should save only one payment record', () => {
+      addPayment(mockCustomerID, mockDate, 20);
+      expect(saveRepayment).toHaveBeenCalledTimes(1);
+    });    
+  });
+  describe('For less than Exact payment', () => {
+    beforeEach(() => {
+      getOutstandingSeason.mockReturnValueOnce({ seasonID: 2011, outstandingAmount: 20 })
+    });
+    it('should record the full payment to the earliest outstanding season', () => {
+      addPayment(mockCustomerID, mockDate, 10);
+      expect(saveRepayment).toHaveBeenCalledWith(10, 2011, mockCustomerID, mockDate);
+    });
+    it('should save only one payment record', () => {
+      addPayment(mockCustomerID, mockDate, 10);
+      expect(saveRepayment).toHaveBeenCalledTimes(1);
+    });    
   });
 });
