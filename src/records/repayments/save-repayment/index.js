640fccc6bfdd97f836bb2d4b450b373e124b7cba
Angello Obel <angello@obel.co>
2020-01-28 18:23:07 +0300

    implemented save repayments

--- src/records/repayments/save-repayment/index.js
+++ src/records/repayments/save-repayment/index.js
@@ -1,3 +1,23 @@
-export default (amount, seasonID, customerID, date, parentID) => {
 
-}
+function fetchRepayments() {
+  return localStorage.getItem('Repayments') || []
+};
+
+function setRepayments(repayments) {
+  localStorage.setItem('Repayments', JSON.stringify(repayments));
+};
+
+const saveRepayment = (amount, seasonID, customerID, date, parentID) => {
+  const repayments = fetchRepayments();
+  repayments.push({
+    RepaymentID: repayments.length + 1,
+    Amount: amount,
+    SeasonID: seasonID,
+    CustomerID: customerID,
+    Date: date,
+    ParentID: parentID || null
+  });
+  setRepayments(repayments);
+};
+export default saveRepayment
+
--- src/records/repayments/save-repayment/test.js
+++ src/records/repayments/save-repayment/test.js
@@ -1,7 +1,39 @@
-import saveRepayment  from '.';
+import saveRepayment from '.';
+const amount = 400;
+const seasonID = 2010;
+const customerID = 42;
+const date = '10/8/2012';
+const parentID = 3;
 
 describe('saveRepayment', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  });
   it('should be a function', () => {
     expect(saveRepayment).toBeInstanceOf(Function);
+  });
+  it('should add an entry to the repayments table', () => {
+    saveRepayment(amount, seasonID, customerID, date, parentID);
+    const repayments = JSON.parse(localStorage.getItem('Repayments'));
+    expect(repayments).toContainEqual({
+      RepaymentID: 1,
+      Amount: amount,
+      SeasonID: seasonID,
+      CustomerID: customerID,
+      Date: date,
+      ParentID: parentID
+    });
+  });
+  it('should mark ParentID as null if its not given', () => {
+    saveRepayment(amount, seasonID, customerID, date);
+    const repayments = JSON.parse(localStorage.getItem('Repayments'));
+    expect(repayments).toContainEqual({
+      RepaymentID: 1,
+      Amount: amount,
+      SeasonID: seasonID,
+      CustomerID: customerID,
+      Date: date,
+      ParentID: null
+    });
   });
 });
