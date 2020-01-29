450ba1a4e399436961ef9bcd8f48982bc34a6abf
Angello Obel <angello@obel.co>
2020-01-29 17:15:40 +0300

    pretty tabs

--- src/App/data-browser/index.js
+++ src/App/data-browser/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomerSummaryTable from './customer-summary-table';
 import RepaymentsTable from './repayments-table';
+import { AppBar, Tabs, Tab } from '@material-ui/core';
 
 const DataBrowser = () => {
+  const [value, setValue] = useState(0);
+  const handleChange = (e, val) => {
+    setValue(val)
+  }
   return (
     <div style={{ padding: 16 }}>
-
-      <CustomerSummaryTable />
-      <RepaymentsTable />
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Repayments" />
+          <Tab label="Customer Summary" />
+        </Tabs>
+      </AppBar>
+      {value === 0 && <RepaymentsTable />}
+      {value === 1 && <CustomerSummaryTable />}
     </div>
   );
 };
