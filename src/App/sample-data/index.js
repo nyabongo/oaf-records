8a331b690dfa4a0e81ed3371364b53005ad14649
Angello Obel <angello@pop-os.localdomain>
2020-01-29 10:35:56 +0300

    Adds Seasons to locacstorage

--- src/App/sample-data/index.js
+++ src/App/sample-data/index.js
@@ -1,13 +1,14 @@
 import data from './data';
 
-const { Customers } = data;
+const { Customers, Seasons } = data;
 
 export function clearData() {
 
 }
 
 export function loadData() {
-  localStorage.setItem('Customers',JSON.stringify(Customers))
+  localStorage.setItem('Customers', JSON.stringify(Customers))
+  localStorage.setItem('Seasons', JSON.stringify(Seasons))
 }
 
 export function uploadPayments() {
