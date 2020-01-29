8ffac32040bd04ec5703f295c00b85f83596cf6a
Angello Obel <angello@obel.co>
2020-01-29 18:35:41 +0300

    created repayments table

--- src/App/data-browser/repayments-table/index.js
+++ src/App/data-browser/repayments-table/index.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  makeStyles,
+} from '@material-ui/core';
 
-const RepaymentsTable = () => (
-  <div>
-      Repayments table
-  </div>
-);
+const useClasses = makeStyles({
+  tableContainer: {
+  },
+  header: {
+    fontWeight: 'bold',
+    '& >*': {
+    },
+  },
+});
+
+function fetchRepayments() {
+  return JSON.parse(localStorage.getItem('Repayments') || '[]');
+}
+const RepaymentsTable = () => {
+  const classes = useClasses();
+  const repayments = fetchRepayments();
+  return (
+    <TableContainer component={Paper} className={classes.tableContainer}>
+      <Table size="small">
+        <TableHead className={classes.header}>
+          <TableRow>
+            <TableCell className={classes.header} align="center">Customer ID</TableCell>
+            <TableCell className={classes.header}>Season ID</TableCell>
+            <TableCell className={classes.header}>Date</TableCell>
+            <TableCell className={classes.header}>Amount</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {repayments.map(({
+            CustomerID, SeasonID, Date: date, Amount,
+          }, i) => {
+            const key = `repayments-${i}-${CustomerID}-${SeasonID}`;
+            return (
+              <TableRow data-testid={`row-${i}`} key={key}>
+                <TableCell align="center">{CustomerID}</TableCell>
+                <TableCell align="left">{SeasonID}</TableCell>
+                <TableCell align="left">{date}</TableCell>
+                <TableCell align="left">{Amount}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default RepaymentsTable;
--- src/App/data-browser/repayments-table/test.js
+++ src/App/data-browser/repayments-table/test.js
@@ -2,12 +2,74 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import RepaymentsTable from '.';
 
+const sampleRepayments = [
+  {
+    CustomerID: 2,
+    SeasonID: 180,
+    Date: '3/4/2016',
+    Amount: 100,
+  },
+  {
+    CustomerID: 3,
+    SeasonID: 0,
+    Date: '3/5/2016',
+    Amount: 300,
+  },
+  {
+    CustomerID: 4,
+    SeasonID: 180,
+    Date: '3/6/2016',
+    Amount: 500,
+  },
+];
 describe('RepaymentsTable', () => {
   let result;
+  beforeAll(() => {
+    localStorage.setItem('Repayments', JSON.stringify(sampleRepayments));
+  });
   beforeEach(() => {
     result = render(<RepaymentsTable />);
   });
   it('should render without crashing', () => {
     expect(result).toBeTruthy();
+  });
+  it('Should have a Customer ID column', () => {
+    expect(result.getByText('Customer ID')).toBeInTheDocument();
+  });
+  it('should have a Season ID column', () => {
+    expect(result.getByText('Season ID')).toBeInTheDocument();
+  });
+  it('should have a Date column', () => {
+    expect(result.getByText('Date')).toBeInTheDocument();
+  });
+  it('should have an Amount column', () => {
+    expect(result.getByText('Amount')).toBeInTheDocument();
+  });
+  sampleRepayments.forEach(({
+    CustomerID, SeasonID, Date: date, Amount,
+  }, i) => {
+    describe(`Row : ${i}`, () => {
+      let row;
+      beforeEach(() => {
+        row = result.getByTestId(`row-${i}`);
+      });
+
+      it('should show the Customer ID', () => {
+        const cell = row.children[0];
+        expect(cell.innerHTML).toEqual(`${CustomerID}`);
+      });
+      it('should show the Season ID', () => {
+        const cell = row.children[1];
+        expect(cell.innerHTML).toEqual(`${SeasonID}`);
+      });
+      it('should show the Date', () => {
+        const cell = row.children[2];
+        expect(cell.innerHTML).toEqual(`${date}`);
+      });
+      it('should show the Amount', () => {
+        const cell = row.children[3];
+        expect(cell.innerHTML).toEqual(`${Amount}`);
+      });
+    });
   });
 });
