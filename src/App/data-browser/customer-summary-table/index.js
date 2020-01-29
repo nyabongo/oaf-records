56e58a665747ee36c8ba81f86bb5a24aca5cdbf0
Angello Obel <angello@pop-os.localdomain>
2020-01-29 14:22:15 +0300

    Shows customer summary headers

--- src/App/data-browser/customer-summary-table/index.js
+++ src/App/data-browser/customer-summary-table/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react"
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell
+} from "@material-ui/core"
 
 const CustomerSummaryTable = () => {
   return (
-    <div>
-      Customer Summary Table
-    </div>
-  );
-};
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Customer ID</TableCell>
+            <TableCell>Season ID</TableCell>
+            <TableCell>Total Repaid</TableCell>
+            <TableCell>Total Credit</TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+    </TableContainer>
+  )
+}
 
-export default CustomerSummaryTable;
+export default CustomerSummaryTable
--- src/App/data-browser/customer-summary-table/test.js
+++ src/App/data-browser/customer-summary-table/test.js
@@ -10,4 +10,16 @@ describe('CustomerSummaryTable', () => {
   it('should render without crashing', () => {
     expect(result).toBeTruthy();
   });
+  it('Should have a Customer ID column', () => {
+    expect(result.getByText('Customer ID')).toBeInTheDocument();
+  });
+  it('should have a Season ID column', () => {
+    expect(result.getByText('Season ID')).toBeInTheDocument();
+  });
+  it('should have a Total Repaid column', () => {
+    expect(result.getByText('Total Repaid')).toBeInTheDocument();
+  });
+  it('should have a Total Credit column', () => {
+    expect(result.getByText('Total Credit')).toBeInTheDocument();
+  });
 });
