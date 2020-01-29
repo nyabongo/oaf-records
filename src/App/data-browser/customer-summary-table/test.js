f531387653f867721303c3682af4a26e018d3e40
Angello Obel <angello@obel.co>
2020-01-29 15:29:13 +0300

    Customer Summary table update

--- src/App/data-browser/customer-summary-table/test.js
+++ src/App/data-browser/customer-summary-table/test.js
@@ -2,8 +2,32 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import CustomerSummaryTable from '.';
 
+const sampleSummary = [
+  {
+    "CustomerID": 1,
+    "SeasonID": 110,
+    "Credit": 7900,
+    "TotalRepaid": 7900
+  },
+  {
+    "CustomerID": 2,
+    "SeasonID": 110,
+    "Credit": 7900,
+    "TotalRepaid": 7900
+  },
+  {
+    "CustomerID": 1,
+    "SeasonID": 130,
+    "Credit": 8000,
+    "TotalRepaid": 7400
+  },
+]
+
 describe('CustomerSummaryTable', () => {
   let result;
+  beforeAll(() => {
+    localStorage.setItem('CustomerSummaries', JSON.stringify(sampleSummary));
+  });
   beforeEach(() => {
     result = render(<CustomerSummaryTable />);
   });
@@ -22,4 +46,29 @@ describe('CustomerSummaryTable', () => {
   it('should have a Total Credit column', () => {
     expect(result.getByText('Total Credit')).toBeInTheDocument();
   });
+  sampleSummary.forEach(({ CustomerID, SeasonID, TotalRepaid, Credit }, i) => {
+    describe('Row : ' + i, () => {
+      let row
+      beforeEach(() => {
+        row = result.getByTestId(`row-${i}`)
+      });
+
+      it('should show the Customer ID', () => {
+        const cell = row.children[0];
+        expect(cell.innerHTML).toEqual(`${CustomerID}`);
+      });
+      it('should show the Season ID', () => {
+        const cell = row.children[1];
+        expect(cell.innerHTML).toEqual(`${SeasonID}`);
+      });
+      it('should show the Total Repaid', () => {
+        const cell = row.children[2];
+        expect(cell.innerHTML).toEqual(`${TotalRepaid}`);
+      });
+      it('should show the Credit', () => {
+        const cell = row.children[3];
+        expect(cell.innerHTML).toEqual(`${Credit}`);
+      });
+    });
+  })
 });
