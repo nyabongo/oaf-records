3e8fc046ab1f0c7fda90a3862d23b203d85a6465
Angello Obel <angello@pop-os.localdomain>
2020-01-29 17:15:40 +0300

    pretty tabs

--- src/App/data-browser/test.js
+++ src/App/data-browser/test.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, } from '@testing-library/react';
 import DataBrowser from '.';
 
 jest.mock('./customer-summary-table',
   () => () => <div data-testid="customer-summary-table" ></div>)
 
-  jest.mock('./repayments-table',
-    () => () => <div data-testid="repayments-table" ></div>)
+jest.mock('./repayments-table',
+  () => () => <div data-testid="repayments-table" ></div>)
 
 describe('DataBrowser', () => {
   let result;
   beforeEach(() => {
     result = render(<DataBrowser />);
   });
-  it('should show the customer summary table', () => {
+  it('should show the customer summary table when the customer summary is clicked', () => {
+    fireEvent.click(result.getByText('Customer Summary'));
     expect(result.getByTestId('customer-summary-table')).toBeInTheDocument()
   });
-  it('should show the repayments table', () => {
-    expect(result.getByTestId('repayments-table')).toBeInTheDocument()    
+  it('should show the repayments table when the repayments is clicked', () => {
+    fireEvent.click(result.getByText('Repayments'));
+    expect(result.getByTestId('repayments-table')).toBeInTheDocument()
   });
 });
